9365a7ae980a06585b8913c7953e0f7108dfbdad
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-07-16 13:07:40 +0100

    updated to compile with the new project structure

--- compile.sh
+++ compile.sh
@@ -4,7 +4,8 @@ clear
 clear
 rm ./ExperimentClient
 
-gcc -Wall -Werror -shared ExperimentLibrary.c -o libExperimentLibrary.dylib
-gcc -Wall -Werror ExperimentClient.c -o ExperimentClient -L. -lExperimentLibrary
+
+gcc -Wall -Werror -shared C/ExperimentLibrary.c -o libExperimentLibrary.dylib
+gcc -Wall -Werror C/ExperimentClient.c -o ExperimentClient -L. -lExperimentLibrary
 
 ./ExperimentClient
